0b6293bcc4f98358d11c774e846f75d6d6cc90b2
Adrianna Pińska <adrianna.pinska@gmail.com>
2019-11-13 16:17:47 -0700

    extended the test script; it now randomly generates a variety of images to convert and also compares the fast and slow versions

--- scripts/convertertest.py
+++ scripts/convertertest.py
@@ -2,92 +2,124 @@
 
 import os
 import sys
-import argparse
+import itertools
+import subprocess
+import warnings
+import random
+
 from astropy.io import fits
 import h5py
 import numpy as np
 from numpy.testing import assert_equal, assert_allclose
 
-parser = argparse.ArgumentParser(description="Test for the HDF5 converter")
-parser.add_argument('filename', help='Converted HDF5 filename')
-parser.add_argument('-o', '--original', help="Original FITS filename. The default is the same name as the HDF5 file with the suffix replaced.")
-args = parser.parse_args()
+def compare_fits_hdf5(fitsname, hdf5name):
 
-hdf5name = args.filename
-fitsname = args.original
+    fitsfile = fits.open(fitsname)
+    hdf5file = h5py.File(hdf5name)
 
-if not fitsname:
-    basefilename, _ = os.path.splitext(hdf5name)
-    fitsname = basefilename + ".fits"
+    fitsdata = fitsfile[0].data
+    hdf5data = hdf5file["0/DATA"]
 
-fitsfile = fits.open(fitsname)
-hdf5file = h5py.File(hdf5name)
+    # CHECK MAIN DATASET
 
-fitsdata = fitsfile[0].data
-hdf5data = hdf5file["0/DATA"]
+    assert_equal(hdf5data, fitsdata, err_msg="Main dataset differs.")
 
-# CHECK MAIN DATASET
+    ndim = fitsdata.ndim
 
-assert_equal(hdf5data, fitsdata, err_msg="Main dataset differs.")
+    if ndim == 2:
+        axis_names = "XY"
+    elif ndim == 3:
+        axis_names = "XYZ"
+    elif ndim == 4:
+        axis_names = "XYZW"
+    else:
+        sys.exit("Error: unsupported image dimensions.")
 
-ndim = fitsdata.ndim
+    axes = {v: k for k, v in enumerate(reversed(axis_names))}
+    dims = {v: fitsdata.shape[k] for k, v in enumerate(reversed(axis_names))}
 
-if ndim == 2:
-    axis_names = "XY"
-elif ndim == 3:
-    axis_names = "XYZ"
-elif ndim == 4:
-    axis_names = "XYZW"
-else:
-    sys.exit("Error: unsupported image dimensions.")
+    # CHECK SWIZZLES
 
-axes = {v: k for k, v in enumerate(reversed(axis_names))}
-dims = {v: fitsdata.shape[k] for k, v in enumerate(reversed(axis_names))}
+    swizzled_name = None
 
-# CHECK SWIZZLES
+    if ndim == 3:
+        swizzled_name = "ZYX"
+    elif ndim == 4:
+        swizzled_name = "ZYXW"
 
-swizzled_name = None
+    if swizzled_name:
+        swizzled_shape = tuple(dims[a] for a in reversed(swizzled_name))    
+        assert swizzled_name in hdf5file["0/SwizzledData"], "No swizzled dataset found."
+        assert hdf5file["0/SwizzledData"][swizzled_name].shape == swizzled_shape, "Swizzled dataset has incorrect dimensions."
 
-if ndim == 3:
-    swizzled_name = "ZYX"
-elif ndim == 4:
-    swizzled_name = "ZYXW"
+    # CHECK STATS
 
-if swizzled_name:
-    swizzled_shape = tuple(dims[a] for a in reversed(swizzled_name))    
-    assert swizzled_name in hdf5file["0/SwizzledData"], "No swizzled dataset found."
-    assert hdf5file["0/SwizzledData"][swizzled_name].shape == swizzled_shape, "Swizzled dataset has incorrect dimensions."
+    stats = ["XY"]
 
-# CHECK STATS
+    if ndim >= 3 and dims["Z"] > 1:
+        stats.append("Z")
+        stats.append("XYZ")
+        
+    for s in stats:
+        assert s in hdf5file["0/Statistics"], "%s statistics missing." % s
+        
+        sdata = hdf5file["0/Statistics"][s]
+        stats_axis = tuple(axes[a] for a in s)
+        
+        def assert_close(stat, func, data=fitsdata):
+            if stat in sdata:
+                assert_allclose(sdata[stat], func(data, axis=stats_axis), rtol=1e-5, err_msg = "%s/%s is incorrect. DIFF: %r" % (s, stat, func(data, axis=stats_axis) - sdata[stat]))
+        
+        assert_close("SUM", np.nansum, fitsdata.astype(np.float64))
+        assert_close("SUM_SQ", np.nansum, fitsdata.astype(np.float64)**2)
+        assert_close("MEAN", np.nanmean)
+        assert_close("MIN", np.nanmin)
+        assert_close("MAX", np.nanmax)
+        
+        assert (np.count_nonzero(np.isnan(fitsdata), axis=stats_axis) == sdata["NAN_COUNT"]).all(), "%s/NAN_COUNT is incorrect." % s
+        
+    # CHECK HISTOGRAMS
 
-stats = ["XY"]
+    # TODO: check for bin size correctness
+    # TODO: check histogram values
+    # TODO: check mipmaps
 
-if ndim >= 3 and dims["Z"] > 1:
-    stats.append("Z")
-    stats.append("XYZ")
+    fitsfile.close()
+    hdf5file.close()
+
+if __name__ == "__main__":
+    for N in (2, 3, 4):
+        for nans in itertools.chain((None, ("image",)), itertools.chain.from_iterable(itertools.combinations(("row", "column", "channel", "stokes"), n) for n in range(1, 4+1))):
+            for nan_density in (0, 33, 66, 100):
+                
+                dims = [random.randint(10, 50), random.randint(10, 50)]
+                if N > 2:
+                    dims.append(random.randint(50, 100))
+                if N > 3:
+                    dims.append(random.randint(1, 4))
+                
+                print("Testing %r image" % (dims,), end="")
+                
+                params = ["make_image.py", "-o", "test.fits"]
+                if nans:
+                    params.extend(("--nans", *(str(n) for n in nans), "--nan-density", str(nan_density), "--"))
+                    print(" with NaNs inserted in random %r with density %d" % (nans, nan_density), end="")
+                params.extend(str(d) for d in dims)
+                
+                print("...")
+                                
+                subprocess.run(params)
+                
+                subprocess.run(["hdf_convert", "-q", "-o", "FAST.hdf5", "test.fits"])
+                subprocess.run(["hdf_convert", "-q", "-s", "-o", "SLOW.hdf5", "test.fits"])
+                
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore", category=RuntimeWarning)
+                    compare_fits_hdf5("test.fits", "FAST.hdf5")
+                
+                h5diff = subprocess.run(["h5diff", "FAST.hdf5", "SLOW.hdf5"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                assert h5diff.returncode == 0, "Fast and slow versions differ."
+                
+    subprocess.run(["rm", "test.fits", "FAST.hdf5", "SLOW.hdf5"])
+                    
     
-for s in stats:
-    assert s in hdf5file["0/Statistics"], "%s statistics missing." % s
-    
-    sdata = hdf5file["0/Statistics"][s]
-    stats_axis = tuple(axes[a] for a in s)
-    
-    def assert_close(stat, func, data=fitsdata):
-        if stat in sdata:
-            assert_allclose(sdata[stat], func(data, axis=stats_axis), rtol=1e-5, err_msg = "%s/%s is incorrect." % (s, stat))
-            
-    assert_close("SUM", np.nansum)
-    assert_close("SUM_SQ", np.nansum, fitsdata**2)
-    assert_close("MEAN", np.nanmean)
-    assert_close("MIN", np.nanmin)
-    assert_close("MAX", np.nanmax)
-    
-    assert (np.count_nonzero(np.isnan(fitsdata), axis=stats_axis) == sdata["NAN_COUNT"]).all(), "%s/NAN_COUNT is incorrect." % s
-    
-# CHECK HISTOGRAMS
-
-# TODO: check for bin size correctness
-# TODO: check histogram values
-
-fitsfile.close()
-hdf5file.close()
