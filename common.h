75585a3966e58e69ce623be9ef66323182002605
Adrianna Pińska <adrianna.pinska@gmail.com>
2020-05-13 02:21:34 +0200

    bumped converter version

--- common.h
+++ common.h
@@ -16,7 +16,7 @@
 
 #define SCHEMA_VERSION "0.3"
 #define HDF5_CONVERTER "hdf_convert"
-#define HDF5_CONVERTER_VERSION "0.1.10"
+#define HDF5_CONVERTER_VERSION "0.1.11"
 
 #define TILE_SIZE (hsize_t)512
 #define MIN_MIPMAP_SIZE (hsize_t)128
