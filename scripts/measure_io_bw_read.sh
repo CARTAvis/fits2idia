8c56a2b3abd13f27779690a43f92ecf2408482f8
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2026-09-25 10:35:56 +0800

    improvements and bugfixes

--- scripts/measure_io_bw_read.sh
+++ scripts/measure_io_bw_read.sh
@@ -21,7 +21,7 @@ rm -f randread_bw_vs_size.txt
 echo
 echo "TESTING randread started at (next read stars at random position):"
 date
-for bs in `echo "4k 64k 1M 16M 32M 64M 128M"`
+for bs in `echo "4k 64k 1M 16M 32M 64M 128M 256M 512M"`
 do
     echo "rm -f ${test_file}"
     rm -f ${test_file}
@@ -29,12 +29,15 @@ do
     # keeping the file for read tests -> no option --unlink=1
     echo "fio --name=iotest --filename=${test_file} --size=${file_size} --bs=${bs} --rw=randread --direct=1 --ioengine=libaio --runtime=300 --time_based > randread_${bs}.out 2>&1"
     fio --name=iotest --filename=${test_file} --size=${file_size} --bs=${bs} --rw=randread --direct=1 --ioengine=libaio --runtime=300 --time_based > randread_${bs}.out 2>&1
-    bw=$(cat randread_${bs}.out | grep WRITE | awk '{gsub("\\(","");gsub("kB\\/s\\),","");print $3;}')
+    bw=$(cat randread_${bs}.out | grep READ | awk '{gsub("\\(","");gsub("kB\\/s\\),","");print $3;}')
     size=$(cat randread_${bs}.out | grep "iotest" | head -1 | awk '{gsub("B-"," ");print $5;}')
     echo "$size $bw" >> randread_bw_vs_size.txt
     
     sleep 2    
 done
+
+cat randread_bw_vs_size.txt | awk '{if(NR==1){gsub("4096","4*1024",$1);$2=$2/1000.00;$2=$2"e6"}else{gsub("Ki","*1024",$1);gsub("Mi","*1024*1024",$1);gsub("\\.0","",$1);$1=$1;gsub("MB/s),","e6",$2);}print "{ "$1" , "$2" },";}' > randread_bw_vs_size.cc
+cat randread_bw_vs_size.txt | awk '{if(NR==1){$2=($2/1000.0)"e6"}else{gsub("MB/s),","",$2); if($1~/Ki/){sub("Ki","",$1);$1=$1*1024}else if($1~/Mi/){sub("Mi","",$1);$1=$1*1024*1024}}; print $1" "$2;}' > randread_bw_vs_size_PLOT.txt
 echo "TESTING randread complated at:"
 date
 sleep 30
@@ -44,7 +47,7 @@ rm -f read_bw_vs_size.txt
 echo
 echo "TESTING read started at (next read starts exectly where the previous finished):"
 date
-for bs in `echo "4k 64k 1M 16M 32M 64M 128M"`
+for bs in `echo "4k 64k 1M 16M 32M 64M 128M 256M 512M"`
 do
     echo "rm -f ${test_file}"
     rm -f ${test_file}
@@ -52,12 +55,15 @@ do
     # keeping the file for read tests -> no option --unlink=1
     echo "fio --name=iotest --filename=${test_file} --size=${file_size} --bs=${bs} --rw=read --direct=1 --ioengine=libaio --runtime=300 --time_based > read_${bs}.out 2>&1"
     fio --name=iotest --filename=${test_file} --size=${file_size} --bs=${bs} --rw=read --direct=1 --ioengine=libaio --runtime=300 --time_based > read_${bs}.out 2>&1
-    bw=$(cat read_${bs}.out | grep WRITE | awk '{gsub("\\(","");gsub("kB\\/s\\),","");print $3;}')
+    bw=$(cat read_${bs}.out | grep READ | awk '{gsub("\\(","");gsub("kB\\/s\\),","");print $3;}')
     size=$(cat read_${bs}.out | grep "iotest" | head -1 | awk '{gsub("B-"," ");print $5;}')
     echo "$size $bw" >> read_bw_vs_size.txt
     
     sleep 2    
 done
+
+cat read_bw_vs_size.txt | awk '{if(NR==1){gsub("4096","4*1024",$1);$2=$2/1000.00;$2=$2"e6"}else{gsub("Ki","*1024",$1);gsub("Mi","*1024*1024",$1);gsub("\\.0","",$1);$1=$1;gsub("MB/s),","e6",$2);}print "{ "$1" , "$2" },";}' > read_bw_vs_size.cc
+cat read_bw_vs_size.txt | awk '{if(NR==1){$2=($2/1000.0)"e6"}else{gsub("MB/s),","",$2); if($1~/Ki/){sub("Ki","",$1);$1=$1*1024}else if($1~/Mi/){sub("Mi","",$1);$1=$1*1024*1024}}; print $1" "$2;}' > read_bw_vs_size_PLOT.txt
 echo "TESTING read complated at:"
 date
 sleep 30
--- scripts/measure_io_bw_write.sh
+++ scripts/measure_io_bw_write.sh
@@ -19,7 +19,7 @@ rm -f randwrite_bw_vs_size.txt
 echo
 echo "TESTING randwrite started at (next write stars at random position):"
 date
-for bs in `echo "4k 64k 1M 16M 32M 64M 128M"`
+for bs in `echo "4k 64k 1M 16M 32M 64M 128M 256M 512M"`
 do
     echo "rm -f ${test_file}"
     rm -f ${test_file}
@@ -28,12 +28,16 @@ do
     echo "fio --name=iotest --filename=${test_file} --size=${file_size} --bs=${bs} --rw=randwrite --direct=1 --ioengine=libaio --runtime=300 --time_based > randwrite_${bs}.out 2>&1"
     fio --name=iotest --filename=${test_file} --size=${file_size} --bs=${bs} --rw=randwrite --direct=1 --ioengine=libaio --runtime=300 --time_based > randwrite_${bs}.out 2>&1
     filefrag -v ${test_file}
-    bw=$(cat randwrite_${bs}.out | grep WRITE | awk '{gsub("\\(","");gsub("kB\\/s\\),","");print $3;}')
+    # gsub("kB\\/s\\),","");gsub("MB\\/s\\),","");
+    bw=$(cat randwrite_${bs}.out | grep WRITE | awk '{gsub("\\(","");print $3;}')
     size=$(cat randwrite_${bs}.out | grep "iotest" | head -1 | awk '{gsub("B-"," ");print $5;}')
     echo "$size $bw" >> randwrite_bw_vs_size.txt
     
     sleep 2    
 done
+
+cat randwrite_bw_vs_size.txt | awk '{if(NR==1){gsub("4096","4*1024",$1);$2=$2/1000.00;$2=$2"e6"}else{gsub("Ki","*1024",$1);gsub("Mi","*1024*1024",$1);gsub("\\.0","",$1);$1=$1;gsub("MB/s),","e6",$2);}print "{ "$1" , "$2" },";}' > randwrite_bw_vs_size.cc
+cat randwrite_bw_vs_size.txt | awk '{if(NR==1){$2=($2/1000.0)"e6"}else{gsub("MB/s),","",$2); if($1~/Ki/){sub("Ki","",$1);$1=$1*1024}else if($1~/Mi/){sub("Mi","",$1);$1=$1*1024*1024}}; print $1" "$2;}' > randwrite_bw_vs_size_PLOT.txt
 echo "TESTING randwrite complated at:"
 date
 sleep 30
@@ -43,7 +47,7 @@ rm -f write_bw_vs_size.txt
 echo
 echo "TESTING write started at (next write starts exectly where the previous finished):"
 date
-for bs in `echo "4k 64k 1M 16M 32M 64M 128M"`
+for bs in `echo "4k 64k 1M 16M 32M 64M 128M 256M 512M"`
 do
     echo "rm -f ${test_file}"
     rm -f ${test_file}
@@ -52,12 +56,17 @@ do
     echo "fio --name=iotest --filename=${test_file} --size=${file_size} --bs=${bs} --rw=write --direct=1 --ioengine=libaio --runtime=300 --time_based > write_${bs}.out 2>&1"
     fio --name=iotest --filename=${test_file} --size=${file_size} --bs=${bs} --rw=write --direct=1 --ioengine=libaio --runtime=300 --time_based > write_${bs}.out 2>&1
     filefrag -v ${test_file}
-    bw=$(cat write_${bs}.out | grep WRITE | awk '{gsub("\\(","");gsub("kB\\/s\\),","");print $3;}')
+    # gsub("kB\\/s\\),","");gsub("MB\\/s\\),","");
+    bw=$(cat write_${bs}.out | grep WRITE | awk '{gsub("\\(","");print $3;}')
     size=$(cat write_${bs}.out | grep "iotest" | head -1 | awk '{gsub("B-"," ");print $5;}')
     echo "$size $bw" >> write_bw_vs_size.txt
     
     sleep 2    
 done
+
+cat write_bw_vs_size.txt | awk '{if(NR==1){gsub("4096","4*1024",$1);$2=$2/1000.00;$2=$2"e6"}else{gsub("Ki","*1024",$1);gsub("Mi","*1024*1024",$1);gsub("\\.0","",$1);$1=$1;gsub("MB/s),","e6",$2);}print "{ "$1" , "$2" },";}' > write_bw_vs_size.cc
+cat write_bw_vs_size.txt | awk '{if(NR==1){$2=($2/1000.0)"e6"}else{gsub("MB/s),","",$2); if($1~/Ki/){sub("Ki","",$1);$1=$1*1024}else if($1~/Mi/){sub("Mi","",$1);$1=$1*1024*1024}}; print $1" "$2;}' > write_bw_vs_size_PLOT.txt
+
 echo "TESTING write complated at:"
 date
 sleep 30
